69061ddf9f003b8aa3ad0031a3a993a6861d9166
Rhys Pollard <rpollard12@hotmail.co.uk>
2021-05-04 17:48:12 +0100

    Feature: Created link to product details page and tested whether data was loading correctly from both model object instances. SMall issue with company logo not loading.

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.all_products, name="products"),
-    path("<int:product_id>/", views.product_details, name="products_details"),
+    path("<int:product_id>/", views.product_details, name="product_details"),
     path("partners/", views.partners, name="partners"),
 ]
