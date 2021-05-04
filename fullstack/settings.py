590601fcfc8664d0e775ec4a47ff5f1555ae6022
Rhys Pollard <rpollard12@hotmail.co.uk>
2021-05-04 17:11:08 +0100

    Updated settings.py and fullstack urls.py with media and static directories for working development environment to show loaded images. Files now placed in media folder.

--- fullstack/settings.py
+++ fullstack/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.media",
             ],
         },
     },
@@ -163,6 +164,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+# Media files directories added for local/development environment.
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
