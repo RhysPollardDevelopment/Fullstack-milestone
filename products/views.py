db2cb558f84c791b7f4e44b7a12bdb9a7d320340
Rhys Pollard <rpollard12@hotmail.co.uk>
2021-05-04 12:48:18 +0100

    Refactor: Refactored all_products view to correctly add context of products

--- products/views.py
+++ products/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def all_products(request):
+    """
+    Returns page which contains all current products for the subscription
+    service.
+    """
+    products = Product.objects.all()
 
-    return render(request, "products/products.html")
+    context = {"products": products}
+    print(context["products"])
+    return render(request, "products/products.html", context)
