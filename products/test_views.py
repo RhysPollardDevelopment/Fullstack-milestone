cd49927b70d2f84c1b4fdc98bf11beac209339ec
Rhys Pollard <rpollard12@hotmail.co.uk>
2021-05-04 13:05:58 +0100

    Test: Added products details test prior to creating view

--- products/test_views.py
+++ products/test_views.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from .models import Product, Company
 
 
 class TestProductViews(TestCase):
@@ -8,3 +9,12 @@ class TestProductViews(TestCase):
         self.assertTemplateUsed(response, "products/products.html")
         # Check that rendered context contains 6 products.
         self.assertEqual(len(response.context["products"]), 6)
+
+    def test_get_product_details(self):
+        product = Product.objects.create(
+            name="Test product",
+            description="Product test description",
+        )
+        response = self.client.get(f"products/{product.id}")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/products.html")
