c0829b633d10d509482a1267cc268184c2ccb4a8
Rhys Pollard <rpollard12@hotmail.co.uk>
2021-05-04 12:40:24 +0100

    Test: Unit test for all_products view, currently fails as no products in context.

--- products/test_views.py
+++ products/test_views.py
@@ -1,1 +1,10 @@
 from django.test import TestCase
+
+
+class TestProductViews(TestCase):
+    def test_get_all_products(self):
+        response = self.client.get("/products/")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "products/products.html")
+        # Check that rendered context contains 6 products.
+        self.assertEqual(len(response.context["products"]), 6)
--- products/views.py
+++ products/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from .models import Product
 
 
 def all_products(request):
-    product = Product.objects.create(name="named", description="description")
-    str(product)
+
     return render(request, "products/products.html")
