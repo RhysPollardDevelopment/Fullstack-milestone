83b66e979428a1099ba8ab253e6cb881d945d75b
Rhys Pollard <rpollard12@hotmail.co.uk>
2021-06-04 15:24:27 +0100

    Test/refactor: Tests created to check that users who have active subscriptions can access the subscriptions base page. Created decorator test to redirect users correct however seems to have broken previous test.

--- subscriptions/test_views.py
+++ subscriptions/test_views.py
@@ -1,7 +1,20 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import StripeSubscription
+from datetime import datetime, timezone
 
 
 class TestSubscriptionViews(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username="testuser",
+            email="testuser@test.com",
+        )
+        # Need to call set_password identified at https://stackoverflow.com/
+        # questions/2619102/djangos-self-client-login-does-not-work-in-unit-tests
+        self.user.set_password("12345")
+        self.user.save()
+
     def test_get_subscriptions_page(self):
         """Test that user can access main subscription page."""
         response = self.client.get("/subscription/")
@@ -10,3 +23,17 @@ class TestSubscriptionViews(TestCase):
             response, "subscriptions/subscriptions_page.html"
         )
         self.assertIsNotNone(response.context["products"])
+
+    def test_subscription_page_redirect_if_subscription_active(self):
+        StripeSubscription.objects.create(
+            subscription_id="testsub",
+            start_date=datetime(2020, 5, 5, 12, 0, 0, tzinfo=timezone.utc),
+            end_date=datetime(2030, 6, 5, 12, 0, 0, tzinfo=timezone.utc),
+            stripe_user=self.user.userprofile,
+        )
+        print(self.user.userprofile.stripe_customer_id)
+        logged_in = self.client.login(username="testuser", password="12345")
+        response = self.client.get("/subscription/")
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, "/")
+        self.assertTemplateNotUsed("subscriptions/subscriptions_page.html")
--- subscriptions/views.py
+++ subscriptions/views.py
@@ -4,19 +4,32 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.conf import settings
+from django.contrib.auth.decorators import user_passes_test
 
 import stripe
 
 
+def not_subscribed(user):
+    print(user)
+    if user.userprofile.has_active_subscription:
+        return False
+    else:
+        return True
+
+
+@user_passes_test(not_subscribed, login_url="/", redirect_field_name=None)
 def subscription_page(request):
+
     if request.user.is_authenticated:
-        if request.user.userprofile.is_subscribed:
+        if request.user.userprofile.has_active_subscription:
             return redirect(reverse("home"))
+
     template = "subscriptions/subscriptions_page.html"
     # This relates to the djstripe product object which is the subscription
     # plan made on the Stripe dashboard.
     stripe.api_key = settings.STRIPE_SECRET_KEY
     products = stripe.Product.list()
+
     context = {
         "products": products,
     }
