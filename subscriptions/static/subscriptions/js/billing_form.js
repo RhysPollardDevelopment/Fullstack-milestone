096c62597ddc426dec22b7e43732a997ed281d58
Rhys Pollard <rpollard12@hotmail.co.uk>
2021-06-04 17:19:26 +0100

    Fix/feature: Corrected structure of forms to correctly load javascript files. Billing_form.js now hides and reveals billing address form.

---
+++ subscriptions/static/subscriptions/js/billing_form.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    bindBilling();
+    formSetup();
+
+})
+
+function formSetup() {
+    let billingForm = document.getElementById("billing-form")
+    let inputs = billingForm.getElementsByTagName("input");
+    for (i = 0; i < inputs.length; i++) {
+        inputs[i].required = false;
+    }
+}
+
+function bindBilling() {
+    let billingCheck = document.getElementById("same-billing");
+    let billingForm = document.getElementById("billing-form")
+    let inputs = billingForm.getElementsByTagName("input");
+
+    if (billingCheck) {
+        billingCheck.addEventListener("click", function () {
+            if (billingCheck.checked) {
+                billingForm.classList.add("d-none");
+                for (i = 0; i < inputs.length; i++) {
+                    inputs[i].required = false;
+                }
+            } else {
+                billingForm.classList.remove("d-none");
+                for (i = 0; i < inputs.length; i++) {
+                    inputs[i].required = true;
+                }
+            }
+        })
+    }
+}
